f44c2079a82218c3d2b1ac4752e73fb495ea9df5
thint2908 <thint2908@gmail.com>
2024-11-07 16:58:53 +0700

    config route

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import { Fragment } from "react"
-import { NavLink } from "react-router-dom"
+import { NavLink, Routes, Route } from "react-router-dom"
 
 import "swiper/scss"
 import MovieList from "./components/movie/MovieList"
 import Banner from "./components/banner/Banner"
+import Header from "./components/layout/Header"
+import Main from "./components/layout/Main"
+import HomePage from "./pages/HomePage"
+import MoviesPage from "./pages/MoviesPage"
 
 function App() {
 	return (
 		<Fragment>
-			<header className="header flex items-center justify-center gap-x-5 text-white py-10 mb-5">
-				<span className="text-primary">Home</span>
-				<span>Movies</span>
-			</header>
-			<Banner></Banner>
-			<section className="movies-layout page-container pb-20">
-				<h2 className="capitalize text-white mb-10 text-3xl font-bold">Now playing</h2>
-				<MovieList></MovieList>
-			</section>
-			<section className="movies-layout page-container pb-20">
-				<h2 className="capitalize text-white mb-10 text-3xl font-bold">Top rated</h2>
-				<MovieList type="top_rated"></MovieList>
-			</section>
-			<section className="movies-layout page-container pb-20">
-				<h2 className="capitalize text-white mb-10 text-3xl font-bold">Trending</h2>
-				<MovieList type="popular"></MovieList>
-			</section>
+			<Routes>
+				<Route element={<Main></Main>}>
+					<Route
+						path="/"
+						element={
+							<>
+								<Banner></Banner>
+								<HomePage></HomePage>
+							</>
+						}
+					></Route>
+					<Route
+						path="/movies"
+						element={<MoviesPage></MoviesPage>}
+					></Route>
+				</Route>
+			</Routes>
 		</Fragment>
 	)
 }
---
+++ src/pages/MoviesPage.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const MoviesPage = () => {
+	return (
+		<div>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sit hic, optio vitae
+			soluta autem explicabo voluptatem quidem recusandae id ducimus ipsam voluptatibus
+			eveniet a illum sequi porro praesentium illo.
+		</div>
+	)
+}
+
+export default MoviesPage
